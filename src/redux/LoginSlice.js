1566c0a595c4a84ac7e839f0ceeb6351e0f4ba85
Sanaa <ahdoursanaa@gmail.com>
2024-01-24 19:06:50 +0100

    commentaires vf

--- src/redux/LoginSlice.js
+++ src/redux/LoginSlice.js
@@ -1,7 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-// Thunk fetchLogin pour gérer la connexion de l'utilisateur
-export const fetchLogin = createAsyncThunk(
+export const fetchLogin = createAsyncThunk( // Thunk fetchLogin pour gérer la connexion de l'utilisateur
   "login/fetchLogin",
   async (credentials, { rejectWithValue }) => {
     try {
@@ -14,18 +13,18 @@ export const fetchLogin = createAsyncThunk(
         body: JSON.stringify(credentials),
       });
 
-      
+
       if (!response.ok) {
         throw new Error("Network response was not ok " + response.statusText);
       }
 
-      
+
       const data = await response.json();
       sessionStorage.setItem("token", data.body.token);
 
       return data;
     } catch (error) {
-      
+
       return rejectWithValue(error.message);
     }
   }
@@ -33,31 +32,31 @@ export const fetchLogin = createAsyncThunk(
 
 
 const initialState = {
-  token: sessionStorage.getItem("token") || null, 
-  status: "idle", 
-  error: null, 
+  token: sessionStorage.getItem("token") || null,
+  status: "idle",
+  error: null,
 };
 
 // Gestion état authentification (login)
 const loginSlice = createSlice({
-  name: "login", 
-  initialState, 
+  name: "login",
+  initialState,
   reducers: {
     resetLoginState: () => initialState,
   },
   extraReducers: (builder) => {
     builder
       .addCase(fetchLogin.pending, (state) => {
-        state.status = "loading"; 
+        state.status = "loading";
       })
       .addCase(fetchLogin.fulfilled, (state, action) => {
         state.status = "succeeded";
-        
+
         state.token = action.payload.body.token;
       })
       .addCase(fetchLogin.rejected, (state, action) => {
-        state.status = "failed"; 
-        state.error = action.error.message; 
+        state.status = "failed";
+        state.error = action.error.message;
       });
   },
 });
