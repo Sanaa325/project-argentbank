dcad5121bcfe04e21a7dded0b00c5b9ba2a43558
Sanaa <ahdoursanaa@gmail.com>
2024-01-24 16:41:03 +0100

    approuter

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,2 +1,17 @@
+//Librairies
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+
+//Components
+
+import Home from "./Pages/Home/Home";
+import SignIn from "./Pages/SignIn/SignIn";
+import User from "./Pages/User/User";
+import Error from "./Pages/Error/Error";
+import PrivateRoute from "./Components/PrivateRoute";
+
+
+
 
 
